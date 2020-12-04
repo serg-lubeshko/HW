8c125576b5906c1b44ad9f2cbab7582d2cf6f3f5
Sergei <posse7297@gmail.com>
2020-12-04 21:57:22 +0300

    Let's work with statics
    1.Created a 'static' folder in our project. I downloaded bootsrap, css there.
    2. Made changes to setting.py for statics.
    3. Loaded static 'loadstatic' into html template
    
    Signed-off-by: Sergei <posse7297@gmail.com>

--- HW/settings.py
+++ HW/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'HW/static')
+]
 # Скажем джанго куда прята/брать медиа
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
